9a937920b95ceef4864ece858942d57617e5a618
Bluesman98 <papaderos1998@gmail.com>
2022-11-06 22:10:19 +0200

    changed image path

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -8,7 +8,7 @@ function Characters(props) {
         return (
           <div key={i}>
             <img
-              src={"./images/"+ item.name + ".png"}
+              src={"../images/"+ item.name + ".png"}
             ></img>
             <div>{item.name}</div>
           </div>
